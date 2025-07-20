c189d94fd6ce24bd09a75587f35de84d177305e3
danotoriousflaco101 <mestreselektor@gmail.com>
2025-07-20 11:53:13 +0200

    refactor: Use @Value injection for production DataSource

--- src/main/java/com/flaco/mycatalog/config/DataSourceConfig.java
+++ src/main/java/com/flaco/mycatalog/config/DataSourceConfig.java
@@ -1,44 +1,42 @@
 package com.flaco.mycatalog.config;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Configuration
-@Profile("production") // Questa configurazione è attiva SOLO per il profilo 'production'
+@Profile("production") //
 public class DataSourceConfig {
+
+    @Value("${DB_HOST}")
+    private String dbHost;
+
+    @Value("${DB_PORT}")
+    private String dbPort;
+
+    @Value("${DB_NAME}")
+    private String dbName;
+
+    @Value("${DB_USER}")
+    private String dbUser;
+
+    @Value("${DB_PASSWORD}")
+    private String dbPassword;
 
     @Bean
     public DataSource dataSource() {
-        // Render fornisce i dettagli di connessione in una singola variabile DATABASE_URL.
-        String databaseUrl = System.getenv("DATABASE_URL");
-        if (databaseUrl == null) {
-            throw new IllegalStateException("La variabile d'ambiente DATABASE_URL non è impostata!");
-        }
+        
+        String jdbcUrl = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName + "?sslmode=require";
 
-        try {
-            URI dbUri = new URI(databaseUrl);
-
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-
-            // Costruiamo l'URL JDBC corretto dal formato di Render.
-            String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-
-            return DataSourceBuilder.create()
-                    .url(jdbcUrl)
-                    .username(username)
-                    .password(password)
-                    .driverClassName("org.postgresql.Driver")
-                    .build();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("URL del database non valido: " + databaseUrl, e);
-        }
+        return DataSourceBuilder.create()
+                .url(jdbcUrl)
+                .username(dbUser)
+                .password(dbPassword)
+                .driverClassName("org.postgresql.Driver")
+                .build();
     }
 }
